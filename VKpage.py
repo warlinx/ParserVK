b88bd86757906f6d19cf4a246891e5f29e7d7746
warlinux <warlinux@yandex.ru>
2021-08-08 12:19:01 +0300

    part 2

--- VKpage.py
+++ VKpage.py
@@ -1,24 +1,22 @@
-# подключение модулей
+# Подключение модулей
 import requests
 import time
 import sqlite3
 
-# токен и id
-token = 'token'
+# Токен и id
+token = '1bdf4fb165d7cfda53459c7ab93f87dead1972c16ca0215709ac4b0b918fe44650b5e79cc101f520455fb'
 page_id = 'dm'
 
 
-# основной код
+# Основной код
 def vk_download(method, parameters, token=token):
     url = 'https://api.vk.com/method/' + method + '?' + parameters + '&access_token=' + token + "&v=5.21"
     response = requests.get(url)
     try:
         return (response.json())['response']
     except:
-        print('Пожалуйста обновите токен, для этого воспользуйтесь ссылкой из файла main.py')
+        print('Пожалуйста, обновите токен, для этого воспользуйтесь ссылкой из файла main.py')
         exit()
-
-
 title_page = vk_download('users.get', 'user_ids=' + page_id)
 print(title_page)
 
@@ -26,8 +24,16 @@ new_id = str(title_page[0]['id'])
 wall = vk_download('wall.get', 'owner_id=' + new_id)
 count_notes = wall['count']
 name_page = title_page[0]['first_name'] + ' ' + title_page[0]['last_name']
-n = int(input('На странице ' + str(count_notes) + ' записей. Сколько скачаем записей? '))
+n = int(input('На странице ' + str(count_notes) + ' Записей. Сколько получаем записей '))
 conn = sqlite3.connect(name_page+'.db')
+cur = conn.cursor()
+cur.executecur.execute("""CREATE TABLE IF NOT EXISTS wall(
+   number TEXT,
+   author TEXT,
+   comments TEXT,
+   likes TEXT,
+   reposts TEXT);
+""")
 for i in range(0, n):
     param = '&count=1&offset=' + str(i)
     note = vk_download('wall.get', 'owner_id=' + new_id + param)
